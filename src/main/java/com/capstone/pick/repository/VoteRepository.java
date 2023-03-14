ab7e0a868db7416d38db7bb4d629f07c9644e89c
JinJuJeong <wjdwlswn731@gmail.com>
2023-03-14 09:44:03 +0900

    #35 - 투표 게시글 카테고리별 보기 구현
    VoteRepository에 findByCategory 메서드를 추가하여 투표 게시글 카테고리별 보기 기능을 구현함

--- src/main/java/com/capstone/pick/repository/VoteRepository.java
+++ src/main/java/com/capstone/pick/repository/VoteRepository.java
@@ -1,9 +1,14 @@
 package com.capstone.pick.repository;
 
 import com.capstone.pick.domain.Vote;
+import com.capstone.pick.domain.constant.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface VoteRepository extends JpaRepository<Vote, Long> {
 
     void deleteByIdAndUser_UserId(Long voteId, String userId);
+
+    List<Vote> findByCategory(Category category);
 }
